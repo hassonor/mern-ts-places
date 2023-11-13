d7d9d99c796ec6dd8936e3d4886947c07f15686f
Or Hasson <hassonor@gmail.com>
2023-11-13 12:23:21 +0200

    decrease timeout to 100

--- frontend/src/shared/components/Navigation/SideDrawer.tsx
+++ frontend/src/shared/components/Navigation/SideDrawer.tsx
@@ -12,12 +12,12 @@ const SideDrawer: FC<SideDrawerProps> = ({children, show, onClick}) => {
     return createPortal(
         <CSSTransition
             in={show}
-            timeout={150}
+            timeout={100}
             classNames={{
                 enter: 'transform -translate-x-full',
-                enterActive: 'translate-x-0 transition ease-out duration-200', // Use ease-out for smoother enter transition
+                enterActive: 'translate-x-0 transition ease-out duration-200',
                 exit: 'transform translate-x-0',
-                exitActive: '-translate-x-full transition ease-in duration-200' // Use ease-in for smoother exit transition
+                exitActive: '-translate-x-full transition ease-in duration-200'
             }}
             mountOnEnter
             unmountOnExit

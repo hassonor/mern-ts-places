a5df85b0313bdb1b62565b758aa7fcc7ee2d85cb
Or Hasson <hassonor@gmail.com>
2023-11-14 16:17:41 +0200

    adding dummy deleting confirm place modal functionality and styles

--- frontend/src/places/components/PlaceItem.tsx
+++ frontend/src/places/components/PlaceItem.tsx
@@ -10,26 +10,54 @@ interface PlaceItemProps {
 }
 
 const PlaceItem: FC<PlaceItemProps> = ({place}) => {
-    const modalRef = useRef<ModalHandles>(null);
+    const mapModalRef = useRef<ModalHandles>(null);
+    const deleteModalRef = useRef<ModalHandles>(null);
 
     const openMapHandler = () => {
-        modalRef.current?.open();
+        mapModalRef.current?.open();
     };
 
     const closeMapHandler = () => {
-        modalRef.current?.close();
+        mapModalRef.current?.close();
+    };
+
+    const openConfirmDeleteHandler = () => {
+        deleteModalRef.current?.open();
+    };
+
+    const cancelDeleteHandler = () => {
+        deleteModalRef.current?.close();
+    };
+
+    const confirmDeletePlaceHandler = () => {
+        deleteModalRef.current?.close();
+        console.log("Deleting...");
     };
 
     return (
         <>
             <Modal
-                ref={modalRef}
+                ref={mapModalRef}
                 title={<h2 className="font-extrabold text-lg">{place.address}</h2>}
                 footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
             >
                 <div className="h-80 w-full">
                     <GoogleMap center={place.location} zoom={16}/>
                 </div>
+            </Modal>
+            <Modal
+                ref={deleteModalRef}
+                title={<h1 className="font-extrabold text-lg">Are you sure?</h1>}
+                footer={
+                    <>
+                        <Button inverse onClick={cancelDeleteHandler}>CANCEL</Button>
+                        <Button danger onClick={confirmDeletePlaceHandler}>DELETE</Button>
+                    </>
+                }
+            >
+                <p className="font-extrabold text-lg">
+                    Do you want to proceed and delete this place? Please note that it can't be undone thereafter.
+                </p>
             </Modal>
             <li className="my-4">
                 <Card className="p-0">
@@ -44,7 +72,7 @@ const PlaceItem: FC<PlaceItemProps> = ({place}) => {
                     <div className="pt-4 text-center border-t border-gray-300 pb-4">
                         <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
                         <Button to={`/places/${place.id}`}>EDIT</Button>
-                        <Button danger>DELETE</Button>
+                        <Button danger onClick={openConfirmDeleteHandler}>DELETE</Button>
                     </div>
                 </Card>
             </li>
